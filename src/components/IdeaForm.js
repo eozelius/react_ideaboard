e6f7f88b254f6c8ad8b0a2e63cf850e8f1c59759
ethan ozelius <e.ozelius@gmail.com>
2017-12-22 10:38:29 -0500

    Bluring out of a new IdeaForm sends an update request to the API

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class IdeaForm extends Component {
 	constructor(props) {
@@ -11,16 +12,52 @@ class IdeaForm extends Component {
 
 	handleInput = (e) => {
 		this.setState({[e.target.name]: e.target.value})
-
 		console.log(JSON.stringify(this.state))
+	}
+
+	handleBlur = (e) => {
+		const idea = {
+	    title: this.state.title,
+	    body: this.state.body
+	  }
+
+	  console.log(`idea: ${idea}`)
+
+	  axios.put(
+	  	`http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
+	  	{
+	  		idea: idea
+	  	})
+	  .then(response => {
+	  	console.log(JSON.stringify(response))
+	  })
+	  .catch(error => {
+	  	console.log(`error: ${error}`)
+	  })
+
+
+
+
 	}
 
 	render() {
 		return (
 			<div className='tile'>
 				<form>
-					<input className='input' value={this.state.title} type='text' name='title' placeholder='title' onChange={this.handleInput} />
-					<textarea className='input' value={this.state.body} name='body' placeholder='description' onChange={this.handleInput} ></textarea>
+					<input 
+						className='input' 
+						value={this.state.title} 
+						type='text' name='title' 
+						placeholder='title' 
+						onChange={this.handleInput}
+						onBlur={this.handleBlur} />
+					<textarea 
+						className='input' 
+						value={this.state.body} 
+						name='body' 
+						placeholder='description' 
+						onChange={this.handleInput}
+						onBlur={this.handleBlur} ></textarea>
 				</form>
 			</div>
 		)
